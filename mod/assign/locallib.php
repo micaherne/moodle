f04974822839446bae68ee6acaa26adfe5a6cbdb
Michael Aherne <michael.aherne@strath.ac.uk>
2014-01-10 12:37:12 +0000

    MDL-46310 assign Prevent blind marking submission notification email being sent to noreplyemail address

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -3510,6 +3510,7 @@ class assign {
 
         $info = new stdClass();
         if ($blindmarking) {
+            $userfrom = clone($userfrom);
             $info->username = get_string('participant', 'assign') . ' ' . $uniqueidforuser;
             $userfrom->firstname = get_string('participant', 'assign');
             $userfrom->lastname = $uniqueidforuser;
