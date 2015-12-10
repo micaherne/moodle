be27a809f09bac71e2a3095d684205df2b9e8cd6
Michael Aherne <michael.aherne@strath.ac.uk>
2015-12-10 14:51:14 +0000

    Half working version

---
+++ lib/classes/versioning.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once($CFG->libdir . '/composer/semver/src/Constraint/ConstraintInterface.php');
+require_once($CFG->libdir . '/composer/semver/src/Constraint/AbstractConstraint.php');
+require_once($CFG->libdir . '/composer/semver/src/Constraint/Constraint.php');
+require_once($CFG->libdir . '/composer/semver/src/Constraint/EmptyConstraint.php');
+require_once($CFG->libdir . '/composer/semver/src/Constraint/MultiConstraint.php');
+require_once($CFG->libdir . '/composer/semver/src/VersionParser.php');
+require_once($CFG->libdir . '/composer/semver/src/Comparator.php');
+require_once($CFG->libdir . '/composer/semver/src/Semver.php');
+
+
+use Composer\Semver\Semver;
+
+class core_versioning extends Semver {
+
+    public static function satisfies($version, $constraints) {
+
+        if (is_integer($constraints)) {
+            $constraints = '>=' . $constraints;
+        }
+
+        if ($constraints == ANY_VERSION) {
+            $constraints = '*';
+        }
+
+        return parent::satisfies($version, $constraints);
+    }
+
+}
