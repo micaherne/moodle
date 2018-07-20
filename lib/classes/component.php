2e10931e8efbaf5a3097910d8eae856d7e0a1ccf
Andrew Nicols <andrew@nicols.co.uk>
2018-07-20 10:18:35 +0800

    MDL-49672 lib: Move minify to composer
    Note: This change may require some additional changes to check for the
    library before attempting to minify so as not to break installation JS.

--- lib/classes/component.php
+++ lib/classes/component.php
@@ -80,8 +80,6 @@ class core_component {
         'MoodleHQ\\RTLCSS' => 'lib/rtlcss',
         'Leafo\\ScssPhp' => 'lib/scssphp',
         'Box\\Spout' => 'lib/spout/src/Spout',
-        'MatthiasMullie\\Minify' => 'lib/minify/matthiasmullie-minify/src/',
-        'MatthiasMullie\\PathConverter' => 'lib/minify/matthiasmullie-pathconverter/src/',
         'IMSGlobal\LTI' => 'lib/ltiprovider/src',
         'Phpml' => 'lib/mlbackend/php/phpml/src/Phpml',
         'PHPMailer\\PHPMailer' => 'lib/phpmailer/src',
