20e48de15b791a283ef992d1daf05856a48be156
Michael Aherne <micaherne@blueyonder.co.uk>
2020-04-08 19:36:48 +0100

    Fix filter/manage.php

--- filter/manage.php
+++ filter/manage.php
@@ -86,7 +86,8 @@ if ($forfilter) {
         print_error('filterdoesnothavelocalconfig', 'error', $forfilter);
     }
     require_once($CFG->dirroot . '/filter/local_settings_form.php');
-    require_once($CFG->dirroot . '/filter/' . $forfilter . '/filterlocalsettings.php');
+    $componentdir = \core_component::get_component_directory('filter_' . $forfilter);
+    require_once($componentdir . '/filterlocalsettings.php');
     $formname = $forfilter . '_filter_local_settings_form';
     $settingsform = new $formname($CFG->wwwroot . '/filter/manage.php', $forfilter, $context);
     if ($settingsform->is_cancelled()) {
