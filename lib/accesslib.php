d6afe9d4a3f4a8b907c4eea498f17023b864b5e0
Michael Aherne <micaherne@blueyonder.co.uk>
2020-04-08 20:01:26 +0100

    Fix lib/accesslib.php

--- lib/accesslib.php
+++ lib/accesslib.php
@@ -7051,7 +7051,7 @@ class context_module extends context {
 
         $subcaps = array();
 
-        $modulepath = "{$CFG->dirroot}/mod/{$module->name}";
+        $modulepath = \core_component::get_component_directory('mod_' . $module->name);
         if (file_exists("{$modulepath}/db/subplugins.json")) {
             $subplugins = (array) json_decode(file_get_contents("{$modulepath}/db/subplugins.json"))->plugintypes;
         } else if (file_exists("{$modulepath}/db/subplugins.php")) {
@@ -7282,7 +7282,8 @@ class context_block extends context {
         if ($blockinstance = $DB->get_record('block_instances', array('id'=>$this->_instanceid))) {
             global $CFG;
             require_once("$CFG->dirroot/blocks/moodleblock.class.php");
-            require_once("$CFG->dirroot/blocks/$blockinstance->blockname/block_$blockinstance->blockname.php");
+            $componentdir = \core_component::get_component_directory('block_' . $blockinstance->blockname);
+            require_once("{$componentdir}/block_$blockinstance->blockname.php");
             $blockname = "block_$blockinstance->blockname";
             if ($blockobject = new $blockname()) {
                 if ($withprefix){
