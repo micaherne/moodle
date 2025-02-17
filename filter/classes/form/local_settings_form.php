04c65ff3663905d3fbaa3a2e050fae694d7674b6
Michael Aherne <michael.aherne@strath.ac.uk>
2025-02-17 12:15:17 +0000

    MDL-84547 filters: Use correct namespace for autoloading on local_settings_form.

--- filter/classes/form/local_settings_form.php
+++ filter/classes/form/local_settings_form.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace core_filters;
+namespace core_filters\form;
 
 use core\context;
 use moodleform;
